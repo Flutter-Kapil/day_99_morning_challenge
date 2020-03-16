46447e6dc67c5d820b5c88c6001fbd8ec7558bf7
kapiljhajhria <Aerospace44>
2020-03-16 09:37:20 +0530

    save 01

--- bin/main.dart
+++ bin/main.dart
@@ -14,7 +14,31 @@
 //   3    2  4   5
 //  You should return 45, as it is (3 + 2) * (4 + 5).
 
+class BinaryTree {
+  var rootNode;
+  var leftBranch;
+  var rightBranch;
+  BinaryTree({this.rootNode = null, this.leftBranch, this.rightBranch = null});
 
+  int result() {
+    if (rootNode == '+') return rightBranch + leftBranch;
+    if (rootNode == '*') return leftBranch * rightBranch;
+    if (rootNode == '/') return int.parse((leftBranch / rightBranch).toString());
+    if (rootNode == '-') return leftBranch-rightBranch;
+  }
+}
+
+int evaluateBt(BinaryTree root) {
+  int res;
+  if(root.leftBranch==null && root.rightBranch==null)
+    return root.rootNode;
+  if (root.leftBranch is int && root.leftBranch is int)
+    return root.result();
+  return root.result();
+}
 
 void main() {
+  BinaryTree b = BinaryTree(rootNode: '+', leftBranch: 2, rightBranch: 3);
+  BinaryTree a = BinaryTree(rootNode: '+', leftBranch: 2, rightBranch: b);
+  print(evaluateBt(a));
 }
